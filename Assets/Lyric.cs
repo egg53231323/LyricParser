8c29512b8f9b7bd4b1f245557803e4ad76740aed
linc <376817512@qq.com>
2017-04-05 14:27:30 +0800

    init

---
+++ Assets/Lyric.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class LyricItem
+{
+    public string mText = "";
+    public Int64 mTimeStamp = 0;
+}
+
+public class Lyric  {
+    public string mTitle = "";
+    public string mArtist = "";
+    public string mAlbum = "";
+    public string mBy = "";
+    public Int64 mOffset = 0;
+    List<LyricItem> mItems = new List<LyricItem>();
+
+    public bool Load(string path)
+    {
+        if (null == path)
+        {
+            LrcLogError("file path is null");
+            return false;
+        }
+        if (!System.IO.File.Exists(path))
+        {
+            LrcLogError(path + " not exist");
+            return false;
+        }
+
+        return true;
+    }
+
+    protected void LrcLog(string msg)
+    {
+        Debug.Log("[Lyric] " + msg);
+    }
+    protected void LrcLogError(string msg)
+    {
+        Debug.LogError("[Lyric] " + msg);
+    }
+}
